832c72e44bbe3c54987004511d4245797c92fad7
ilkeryasinaksoy <ilkeryasinaksoy@gmail.com>
2017-09-09 05:37:01 +0300

    added server codes.

---
+++ server/sendNotification.php
@@ -0,0 +1,59 @@
+<?php
+
+ function sendNotification($tokens, $message) { 	
+
+ 	 $url = 'https://fcm.googleapis.com/fcm/send';
+
+ 	 $fields = array(
+ 	 	'registration_ids' => $tokens,
+ 	 	'data' => $message);
+
+ 	 $headers = array(
+ 	 	'Authorization:key = SERVER_API_KEY',
+ 	 	'Content-Type: application/json');
+
+ 	  $ch = curl_init();
+ 	  curl_setopt($ch, CURLOPT_URL, $url);
+ 	  curl_setopt($ch, CURLOPT_POST, true);
+ 	  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+ 	  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+ 	  curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);
+ 	  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+ 	  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+
+ 	  $result = curl_exec($ch);
+
+ 	  if($result === FALSE){
+
+ 	  	die('Curl failed: ' . curl_error($ch));
+ 	  }
+
+ 	   curl_close($ch);
+
+ 	   return $result;
+ }
+
+ $conn = mysqli_connect("localhost","root","","fcm");
+
+ $sql = "select token from users";
+
+ $result = mysqli_query($conn,$sql);
+
+ $tokens = array();
+
+ if(mysqli_num_rows($result) > 0 ){
+
+ 	while ($row = mysqli_fetch_assoc($result)){
+
+ 		$tokens[] = $row["token"];
+ 	}
+ }
+
+ mysqli_close($conn);
+
+ $message = array("message" => "This is a test notification!");
+
+ $message_status = sendNotification($tokens, $message);
+
+ echo $message_status; 
+ ?>
